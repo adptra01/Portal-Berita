06948b466476e546924e1d1215d63325bd7f364b
Adi Putra <m.a.syaputra.94@gmail.com>
2024-03-23 03:00:59 +0700

    add setting profile

--- resources/views/components/guest-layout.blade.php
+++ resources/views/components/guest-layout.blade.php
@@ -7,7 +7,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    @yield('seo')
+    <!-- SEO -->
+   
 
     <!-- CDN CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -64,6 +65,7 @@
 
 <body>
     @livewire('partials.brand')
+
     <header>
         <!-- Header Start -->
         <div class="header-area">
